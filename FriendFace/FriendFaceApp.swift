b13a91414c80e09810773b27f525253268e05923
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-10-19 12:27:00 -0500

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Justin Hold on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
